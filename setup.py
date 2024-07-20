24e7efa4c9659f9167cbd1deedfe4de43a3c12ae
Muafira <muafirathasnikt@gmail.com>
2024-07-20 17:10:40 +0400

    version changed - patch added

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='la-rag',
-    version='0.1.0',
+    version='0.1.1',
     description='Layout Aware RAG',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
