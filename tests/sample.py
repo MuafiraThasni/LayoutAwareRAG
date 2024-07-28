045ee3c0a1d83169067f9a50abee99bbf05114e3
Muafira <muafirathasnikt@gmail.com>
2024-07-28 16:57:17 +0400

    sample edited

--- tests/sample.py
+++ tests/sample.py
@@ -1,11 +1,12 @@
 #%%
-import sys
-sys.path.append(r"C:\Users\User\Desktop\LayoutAwareRAG\src\la_rag")
-import pymupdf
-from pipeline import extractInfo as model
+# import sys
+# sys.path.append(r"C:\Users\User\Desktop\LayoutAwareRAG\src\la_rag")
+# import pymupdf
+from la_rag.pipeline import extractInfo as model
+
 #%%
-path = r"C:\Users\User\Desktop\personal\resume_pdf_sample.pdf"
-name = "Resume"
+path = r"C:\Users\User\Desktop\Data Science Matrials\Introduction to Machine Learning with Python ( PDFDrive.com )-min.pdf"
+name = "ml book"
 
 result = model([path],[name],"experience")
 # %%
